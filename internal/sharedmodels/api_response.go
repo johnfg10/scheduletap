d324b3859bc175d7cb5921e6bf21e1cb9b7deea3
Johnfg10 <5445550+johnfg10@users.noreply.github.com>
2020-02-16 20:15:18 +0000

    added api response autofill

---
+++ internal/sharedmodels/api_response.go
@@ -0,0 +1,31 @@
+package sharedmodels
+
+// APIResponse a
+type APIResponse struct {
+	Type        string `json:"type"`
+	Description string `json:"description"`
+}
+
+func NewApiResponse(Type string, description string) APIResponse {
+	response := APIResponse{}
+	response.Type = Type
+	response.Description = description
+	return response
+}
+
+// NewErrorResponse creates a new APIResponse with the error type
+func NewErrorResponse(description string) APIResponse {
+	return NewApiResponse("error", description)
+}
+
+func NewSucessResponse(description string) APIResponse {
+	return NewApiResponse("success", description)
+}
+
+func NewNotFoundResponse(description string) APIResponse {
+	return NewApiResponse("notfound", description)
+}
+
+func NewInternalErrorResponse(err error) APIResponse {
+	return NewApiResponse("interalerror", err.Error())
+}
