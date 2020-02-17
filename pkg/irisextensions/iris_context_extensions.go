0e30906ff3c9590e3dc8be9f1bc00dc30a374dbe
Johnfg10 <5445550+johnfg10@users.noreply.github.com>
2020-02-17 10:25:21 +0000

    created irisextensions to help with common response

---
+++ pkg/irisextensions/iris_context_extensions.go
@@ -0,0 +1,50 @@
+package irisextensions
+
+import "github.com/kataras/iris"
+
+var (
+	sendPrivateDetails bool
+)
+
+func init() {
+	sendPrivateDetails = false
+}
+
+// SendPrivateDetails sets the private field sendPrivateDetails which enabled sending error information to the end user
+// Primarly used for development you almost certainly done want this in production
+func SendPrivateDetails(sendDetails bool) {
+	sendPrivateDetails = sendDetails
+}
+
+// IsErrorPresent takes a error and returns a boolean if there is a error
+func IsErrorPresent(err error) bool {
+	if err != nil {
+		return true
+	}
+	return false
+}
+
+// FinnishOnError finnishs the current request with a json error message
+func FinnishOnError(ctx iris.Context, err error, statuscodeOptional int) bool {
+	if IsErrorPresent(err) {
+		ctx.Application().Logger().Error(err)
+		var resp APIResponse
+
+		if sendPrivateDetails {
+			resp = NewErrorResponse(err.Error())
+		} else {
+			resp = NewErrorResponse("Error details have been logged in the application logger")
+		}
+
+		ctx.JSON(&resp)
+
+		if statuscodeOptional != 0 {
+			ctx.StatusCode(statuscodeOptional)
+		} else {
+			ctx.StatusCode(500)
+		}
+
+		return true
+	}
+	return false
+}
